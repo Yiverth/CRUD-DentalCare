946cd44a1d808652f7859724cca9221840bb561c
YiverthMartinez <yivermartinez@gmail.com>
2024-05-30 21:09:57 -0500

    Tercer commit, se crea clase donde se crean los componentes de la interfaz, se ejecuta y se estblece la conexion de la clase principal haciendo uso de los metodos para el CRUD

---
+++ src/main/java/com/yiverthdevs/dentalcare/listarRegistroUsuario.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yiverthdevs.dentalcare;
+
+/**
+ *
+ * @author HP
+ */
+public class listarRegistroUsuario {
+    
+    // Variables para almacenar los datos que se van a listar 
+    String idregistrousuario;
+    String nombreusuario;
+    String email;
+    String telefono;
+    String contrseña;
+    String genero;
+    
+    // Se guardan cada dato que se desea listar, para almacenarlos y mostrarlos en la interfaz
+
+    public String getIdregistrousuario() {
+        return idregistrousuario;
+    }
+
+    public void setIdregistrousuario(String idregistrousuario) {
+        this.idregistrousuario = idregistrousuario;
+    }
+
+    public String getNombreusuario() {
+        return nombreusuario;
+    }
+
+    public void setNombreusuario(String nombreusuario) {
+        this.nombreusuario = nombreusuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrseña() {
+        return contrseña;
+    }
+
+    public void setContrseña(String contrseña) {
+        this.contrseña = contrseña;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getString(String nombreusuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
